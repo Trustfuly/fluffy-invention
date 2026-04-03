ff687a84229bee60e39b74ac40b2336629c4754b
Trustfuly <58312085+Trustfuly@users.noreply.github.com>
2026-04-03 04:40:50 +0300

    Update yopass.sh

--- ct/yopass.sh
+++ ct/yopass.sh
@@ -15,7 +15,6 @@ var_disk="2"
 var_os="debian"
 var_version="12"
 var_unprivileged="1"
-var_install="yopass"
 
 INSTALL_URL="https://raw.githubusercontent.com/Trustfuly/fluffy-invention/main/install/yopass-install.sh"
 
@@ -36,10 +35,12 @@ function update_script() {
 }
 
 start
-build_container
+#build_container
+build_container 2>&1 | grep -v "curl: (22)"
 pct exec "$CTID" -- mkdir -p /etc/systemd/system/container-getty@1.service.d
 pct exec "$CTID" -- bash -c "printf '[Service]\nExecStart=\nExecStart=-/sbin/agetty --autologin root --noclear tty1\n' > /etc/systemd/system/container-getty@1.service.d/autologin.conf"
-pct exec "$CTID" -- passwd -d root 2>/dev/null
+#pct exec "$CTID" -- passwd -d root 2>/dev/null
+pct exec "$CTID" -- passwd -d root >/dev/null 2>&1
 pct exec "$CTID" -- systemctl daemon-reload
 pct exec "$CTID" -- systemctl restart container-getty@1
 
